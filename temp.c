dbcfa899c9cd2e82861f41292403d3aeb99e99e8
leanne grevenitz <leanne38grevenitz@gmail.com>
2019-04-23 02:14:32 +0000

    first lay out of the caesar cipher

--- temp.c
+++ temp.c
@@ -1,5 +1,22 @@
-#include <stdio.h>
-int main(){
+/*ENGG1003 caesar cipher encription program.
+this code will input text and a key and encript*/
+#include<stdio.h>
 
-return 0;
+void cipher(){ // creat main function for encription here
+        
+}
+
+int main(){
+    
+    int key;
+    char text[101];
+    
+    printf("Enter message here:");
+    scanf("%s", &text);
+    printf("enter key here:");
+    scanf("%d", &key);
+    
+    
+    return 0;
+
 }
