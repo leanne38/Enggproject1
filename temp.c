416a2f0afee8c878c6705cba9396986bb9601a4f
leanne grevenitz <leanne38grevenitz@gmail.com>
2019-04-26 02:06:51 +0000

    rotation cipher

--- temp.c
+++ temp.c
@@ -1,44 +1,36 @@
-/*Cipher Rotation code.this come will take a message and a rotaion key
-from the user and encript that message by moving the letters along depending 
-on what the cipher key is. Two functions; the main function, this gets the 
-input and provides the output to the user. The cipher function, which does the 
-encripting of the message. */ 
-
 #include <stdio.h>
-#include <stdlib.h>
 int main() 
 {
     int x, i; 
-    char message[100], cha; 
+    char message[100]; 
     
     printf("Enter message here:"); 
     scanf("%s", message);
     printf("Enter key here:");
-    scanf("%d", x);
-    switch(x)
+    scanf("%d", &x);
     
-    {
-        //message[100] = cha;
-        
-        case 1:
-        
-        
-        if (message[i] >= 'a' && message[i] <= 'z')
-                if ((message[i] + x) > 122)
-           printf("%s", message[i] + x - 122 + 96);
-        
-           // for (i = 0; cha != '\0'; ++i);
-           // cha = cha + x;
-            //message[i] = cha;
-        break;
+    for(i = 0; message[i] != '\0'; ++i)
+		
+		
+		if(message[i] >= 'a' && message[i] <= 'z')
+		{
+			message[i] = message[i] + x;
+		    
+		}
+			
+			if(message[i] > 'z')
+			{
+				message[i] = message[i] - 'z' + 'a' - 1;
+			}
+          
     
-        default: printf("Error");
-        
-    }
-    
-   // printf("Encripted message, %s", message);
+    printf("Encripted message, %s", message);
     
     return 0;
-    
-}
+}  
+
+
+
+
+
     
